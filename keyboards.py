0029d9aafda8472b612fd4e34413eaee6278a883
osadchafinset-spec <osadchafinset@gmail.com>
2026-05-04 13:29:25 +0200

    Create keyboards.py

---
+++ keyboards.py
@@ -0,0 +1,12 @@
+
+Copy
+
+from telegram import ReplyKeyboardMarkup
+from config import RESET_BTN
+ 
+ 
+def make_keyboard(options: list) -> ReplyKeyboardMarkup:
+    """Клавіатура з кнопкою скидання внизу."""
+    keyboard = [[opt] for opt in options] + [[RESET_BTN]]
+    return ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+ 
