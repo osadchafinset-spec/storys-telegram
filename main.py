23db9ccfbe7768284c40a400b7f9d06b7a4be542
osadchafinset-spec <osadchafinset@gmail.com>
2026-05-04 19:16:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 def main() -> None:
     app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(CommandHandler("reset", reset))
+   
     app.add_handler(MessageHandler(filters.PHOTO, handle_photo))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
     print("✅ Бот запущений")
